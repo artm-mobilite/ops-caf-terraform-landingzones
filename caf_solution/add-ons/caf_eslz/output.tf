e636b5934a89e90546eb0ab5c70e196344e71764
Alexandre Chatelain <6085940+chatelain-io@users.noreply.github.com>
2023-11-08 01:04:54 -0500

    feat: Add resource_groups to output objects

--- caf_solution/add-ons/caf_eslz/output.tf
+++ caf_solution/add-ons/caf_eslz/output.tf
@@ -23,6 +23,9 @@ output "objects" {
             for key, value in module.enterprise_scale.azurerm_private_dns_zone.connectivity : value.name => value
           }
           #
+          "resource_groups" = {
+            for key, value in flatten([for zone_key, zone_value in module.enterprise_scale.azurerm_resource_group : [for rg_key, rg_value in zone_value : rg_value] if zone_value != {}]) : value.name => value
+          }
           "virtual_network_gateways" = {
             for key, value in module.enterprise_scale.azurerm_virtual_network_gateway.connectivity : value.name => value
           }
